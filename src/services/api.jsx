d413f8f824471d075e6ce67716c5bdc5129e9ada
Shubhankar sinha <141034953+Shubhankarkmr@users.noreply.github.com>
2025-09-15 00:37:37 +0530

    Update api.jsx

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create Axios instance
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://backend-hvgn.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
